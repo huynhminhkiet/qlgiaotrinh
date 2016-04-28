0788fe82e0490423e8f0002fedc942fb3a72fdda
huynhminhkiet <huynhminhkiet@gmail.com>
2016-04-28 22:25:12 +0700

    Cat giao dien admin

---
+++ footer.php
@@ -0,0 +1,36 @@
+<section id="footer_bottom_area">
+			<div class="clearfix wrapper footer_bottom">
+				<div class="clearfix copyright floatleft">
+					<p> Copyright &copy; All rights reserved by <span>HuKi Company</span></p>
+				</div>
+			</div>
+		</section>
+		
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.0.min.js"></script>	
+		<script type="text/javascript" src="js/jquery.tooltipster.min.js"></script>		
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$('.tooltip').tooltipster();
+			});
+		</script>
+		 <script type="text/javascript" src="js/selectnav.min.js"></script>
+		<script type="text/javascript">
+			selectnav('nav', {
+			  label: '-Navigation-',
+			  nested: true,
+			  indent: '-'
+			});
+		</script>		
+		<script src="js/pgwslider.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$('.pgwSlider').pgwSlider({
+					
+					intervalDuration: 5000
+				
+				});
+			});
+		</script>
+		<script type="text/javascript" src="js/placeholder_support_IE.js"></script>
+		
+	</body>
---
+++ right-admin.php
@@ -0,0 +1,16 @@
+<div class="clearfix sidebar_container floatright">
+				
+					<div class="clearfix sidebar">
+				
+						<div class="clearfix single_sidebar category_items">
+							<h2>Danh mục</h2>
+							<ul>
+								<li class="cat-item"><a href="">Quản lý khoa</a>></li>
+								<li class="cat-item"><a href="">Học phần</a>></li>
+								<li class="cat-item"><a href="">Quản lý giáo trình</a>></li>
+								<li class="cat-item"><a href="">Quản lý admin</a>></li>
+							</ul>
+						</div>
+			
+					</div>
+				</div>
